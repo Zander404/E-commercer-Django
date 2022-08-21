3e00956c15e4519ece28bc767faa07cf92bd427d
Zander404 <alexandrearthurpg@gmail.com>
2022-08-21 18:50:05 -0300

    add page 404

--- src/app/app/settings.py
+++ src/app/app/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+from cgitb import handler
 import os
+
 import django_heroku
 from pathlib import Path
 
@@ -24,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8mo@hfy!y1@!&ci)6kythpq*31#ojffd5nq56wq^(wfbv-86*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+WhiteNoise = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost:8000"]
 
 
 # Application definition
@@ -47,12 +50,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -130,13 +135,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
+MEDIA_URL = 'static/img/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/img/')
 
 
 
@@ -146,5 +153,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+handler404 = 'app.views.page_not_found_view'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
---
+++ src/app/staticfiles/js/cart.js
@@ -0,0 +1,62 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for(var i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var produtoId = this.dataset.produto
+        var action = this.dataset.action
+        console.log('produtoId:', produtoId, 'action:', action)
+
+        console.log('USER:', user)
+        if (user == 'AnonymousUser') {
+            adicionarCookieItem( produtoId, action)
+            } else {
+                updatePedidoUsuario(produtoId, action)
+            }
+        }) 
+
+}
+
+function adicionarCookieItem(produtoId, action){
+    console.log('Usuario não logado, adicionando item no cookie')
+    if (action == 'add') {
+        if (cart[produtoId] === undefined) {
+            cart[produtoId] = {'quantidade': 1}
+        } else {
+            cart[produtoId]['quantidade'] += 1
+        }
+    }
+
+    if (action == 'remove') {
+        cart[produtoId]['quantidade'] -= 1
+        if (cart[produtoId]['quantidade'] <= 0) {
+            console.log("Removendo produto do carrinho")
+            delete cart[produtoId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+}
+
+function updatePedidoUsuario(produtoId, action){
+    console.log('Usuario logado, atualizando pedido')
+
+    var url = '/update_item/'
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({
+            'produtoId': produtoId,
+            'action': action,
+        })
+    }).then(response =>
+        response.json()
+    ).then(data => {
+        console.log('data:', data);
+        location.reload()
+    })
+
+}
